5973c8142101a1970999f735161896ac6202b175
AndrewMKaiser <andrew.m.kaiser77@gmail.com>
2024-04-12 22:34:21 -0600

    fix MovieSchema in reviews

--- models/Reviews.js
+++ models/Reviews.js
@@ -1,6 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var MovieSchema = require('./Movies');
+
+const MovieSchema = new mongoose.Schema({
+    title: { type: String, required: true, index: true },
+    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+    genre: {
+      type: String,
+      enum: [
+        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      ],
+    },
+    actors: [{
+      actorName: String,
+      characterName: String,
+    }],
+});
 
 const reviewSchema = new Schema({
     movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
